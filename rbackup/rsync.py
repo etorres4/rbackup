eab5bb108ce944e6bde33f040d577a2236bdfe5c
Eric Torres <erictorres4@protonmail.com>
2019-03-17 09:38:32 -0700

    Add logging support for rsync module

--- rbackup/rsync.py
+++ rbackup/rsync.py
@@ -3,12 +3,16 @@
 .. module:: rbackup.rsync
     :synopsis: helper functions for running the rsync backend
 """
+import logging
 import subprocess
 
 
 # ========== Constants ==========
 _RSYNC_BIN = "/usr/bin/rsync"
 
+
+# ========== Logging Setup ===========
+syslog = logging.getLogger(__name__)
 
 # ========== Functions ==========
 def rsync(*args):
@@ -19,4 +23,14 @@ def rsync(*args):
     :raises: subprocess.CalledProcessError if rsync process failed
     """
     cmd = [_RSYNC_BIN, *args]
-    subprocess.run(cmd, capture_output=True, check=True)
+
+    syslog.debug(f"rsync command: {cmd}")
+    syslog.info("Beginning rsync process")
+
+    try:
+        process = subprocess.run(cmd, capture_output=True, check=True)
+    except subprocess.CalledProcessError as e:
+        raise e
+
+    syslog.debug(process.stdout)
+    syslog.info("Process complete")
