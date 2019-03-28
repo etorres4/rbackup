0254a2fa2ec189d30d540c1dd6aa7dd950837fc4
Eric Torres <erictorres4@protonmail.com>
2019-03-28 12:30:05 -0700

    Revert "Add unimplemented test case"
    This reverts commit c132d1c9d874ac0a43db94a2b0e0308cfa5f2bc9.

--- rbackup/tests/test_snapshot.py
+++ rbackup/tests/test_snapshot.py
@@ -6,8 +6,8 @@ Unit tests for the Snapshot class.
 import doctest
 import unittest
 
+from pathlib import Path
 from rbackup.hierarchy.snapshot import Snapshot
-from unittest.mock import patch
 
 # ========== Constants ==========
 TESTING_MODULE = "rbackup.hierarchy.snapshot"
@@ -17,13 +17,3 @@ TESTING_MODULE = "rbackup.hierarchy.snapshot"
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(TESTING_MODULE))
     return tests
-
-
-# ========= Classes ==========
-class TestSnapshot(unittest.TestCase):
-    def setUp(self):
-        self.patched_path = patch(f"{TESTING_MODULE}.Hierarchy")
-        self.mocked_path = self.patched_path.start()
-
-    def tearDown(self):
-        self.patched_path.stop()
