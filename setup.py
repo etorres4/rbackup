4651edecbf16a747e5ebfc4b6f90cb4c00f439b7
Eric Torres <erictorres4@protonmail.com>
2019-04-10 19:28:53 -0700

    Update packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 # ========== Constants ==========
-PACKAGES = ["rbackup", "rbackup.struct", "package_managers"]
+PACKAGES = ["rbackup", "rbackup.package_managers", "rbackup.struct"]
 SCRIPTS = ["bin/backup"]
 
 # ========== Functions ==========
