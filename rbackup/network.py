841e05c3def1cbf2225bb499ca364c0c3738f8db
Eric Torres <erictorres4@protonmail.com>
2019-04-23 23:16:34 -0700

    Add rbackup.network module

---
+++ rbackup/network.py
@@ -0,0 +1,17 @@
+"""
+.. moduleauthor:: Eric Torres
+.. module:: rbackup.network
+    :synopsis: library for interacting with rbackup over the network
+"""
+
+import logging
+
+
+# ========== Constants ==========
+
+
+# ========== Logging Setup ===========
+syslog = logging.getLogger(__name__)
+
+
+# ========== Functions ==========
